1acc869a2a6d855160a145081dda213a3fdf2b76
ThitipatP <thitipat.pengruean@microchip.com>
2022-11-10 09:31:01 +0700

    first commit

--- express/server.js
+++ express/server.js
@@ -13,6 +13,8 @@ router.get('/', (req, res) => {
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
+router.get('/testV1', (req, res) => res.json({ value : "test1" }));
+
 
 app.use(bodyParser.json());
 app.use('/.netlify/functions/server', router);  // path must route to lambda
